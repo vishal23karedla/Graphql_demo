8f464bc9224a4a47ec3f0274c95a5f3d8e211815
vishal23karedla <vishal23karedla@gmail.com>
2022-11-10 17:58:11 +0530

    initial commit

---
+++ src/main/java/com/example/GqlDemo/GqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.GqlDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GqlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GqlDemo/GqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GqlDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
